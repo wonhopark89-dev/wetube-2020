36bbad79d77c2d53cc88f3133d2f9a4e7c20f0be
wonhopark89-dev <wonhopark89.dev@gmail.com>
2020-03-31 00:16:54 +0900

    express setting

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
+const express = require("express");
+const app = express();
 
+// app.get("/", function(req, res) {
+//   res.send("hello");
+// });
+
+const PORT = 4000;
+
+function handleListening() {
+  console.log(`Listening on: http://localhost:${PORT}`);
+}
+
+app.listen(PORT, handleListening);
