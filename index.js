ad918bf024e207f86fe5b6c6af57bc0e9c0a31ce
wonhopark89-dev <wonhopark89.dev@gmail.com>
2020-03-31 23:24:48 +0900

    sample add express use ( middleware )

--- index.js
+++ index.js
@@ -1,20 +1,19 @@
 import express from "express";
 const app = express();
 
-// app.get("/", function(req, res) {
-//   res.send("hello");
-// });
-
 const PORT = 4000;
-
 const handleListening = () =>
   console.log(`Listening on: http://localhost:${PORT}`);
 
 const handleHome = (req, res) => res.send("hello ! from home");
-
 const handleProfile = (req, res) => res.send("you are on profile");
+const handleBetween = (req, res, next) => {
+  console.log("between ~ ");
+  next();
+};
 
+app.use(handleBetween);ç
 app.get("/profile", handleProfile);
-app.get("/", handleHome);
+app.get("/", handleBetween, handleHome);
 
 app.listen(PORT, handleListening);
