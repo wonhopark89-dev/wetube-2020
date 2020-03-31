8b674d8e2e4226af688965f94b42b72ea263d107
wonhopark89-dev <wonhopark89.dev@gmail.com>
2020-03-31 23:48:14 +0900

    add sth kind of middleware package for express

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 import express from "express";
+import morgan from "morgan";
+import helmet from "helmet";
+import cookeParser from "cookie-parser";
+import bodyParser from "body-parser";
+
 const app = express();
 
 const PORT = 4000;
@@ -7,13 +12,15 @@ const handleListening = () =>
 
 const handleHome = (req, res) => res.send("hello ! from home");
 const handleProfile = (req, res) => res.send("you are on profile");
-const handleBetween = (req, res, next) => {
-  console.log("between ~ ");
-  next();
-};
 
-app.use(handleBetween);ç
+// middle-ware -> 위에서 부터 순서대로 진행 
+app.use(cookeParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true })); // from inform
+app.use(morgan("combined"));
+app.use(helmet());
+
 app.get("/profile", handleProfile);
-app.get("/", handleBetween, handleHome);
+app.get("/", handleHome);
 
 app.listen(PORT, handleListening);
