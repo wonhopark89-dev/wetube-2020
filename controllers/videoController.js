52062880e9f7aa5f3cb69240eed23d199aa0cfe6
wonhopark89-dev <wonhopark89.dev@gmail.com>
2020-04-08 22:01:10 +0900

    post body from request on inform

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -11,7 +11,7 @@ export const search = (req, res) => {
     query: { term: searchingBy },
   } = req;
   // const searchingBy = req.query.term // old js format
-  res.render("search", { pageTitle: "Search", searchingBy });
+  res.render("search", { pageTitle: "Search", searchingBy, videos });
 };
 export const upload = (req, res) =>
   res.render("upload", { pageTitle: "Upload" });
--- routers/globalRouter.js
+++ routers/globalRouter.js
@@ -1,13 +1,20 @@
 import express from "express";
 import routes from "../routers";
 import { home, search } from "../controllers/videoController";
-import { login, logout, join } from "../controllers/userController";
+import {
+    getJoin,
+    login,
+    logout,
+    postJoin
+  } from "../controllers/userController";
 
 const globalRouter = express.Router();
 
+globalRouter.get(routes.join, getJoin);
+globalRouter.post(routes.join, postJoin);
+
 globalRouter.get(routes.home, home);
 globalRouter.get(routes.search, search);
-globalRouter.get(routes.join, join);
 globalRouter.get(routes.login, login);
 globalRouter.get(routes.logout, logout);
 
